65dd4b2f8786bab43198ba5f388307864f9d0548
Beela303 <devbeela@gmail.com>
2026-05-26 01:53:56 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,14 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Redirects local api requests to Vercel dev
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
